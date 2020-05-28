84af2afb2c80ce5285ab9c535ee46e456d6f5e07
mrzepeckii <m.rzepaa@gmail.com>
2020-05-28 16:26:15 +0200

    Mover and Player classes functionality has been added

--- TheExpedition2/Player.cs
+++ TheExpedition2/Player.cs
@@ -1,12 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TheExpedition2
 {
-    class Player
+    class Player : Mover
     {
+        private Weapon equippedWeapon;
+        public int HitPoints { get; private set; }
+        private List<Weapon> inventory = new List<Weapon>();
+        public IEnumerable<string> Weapons
+        {
+            get
+            {
+                List<string> names = new List<string>();
+                foreach (Weapon weapon in inventory)
+                    names.Add(weapon.Name);
+                return names;
+            }
+        }
+
+        public Player(Game game, Point location)
+            : base(game, location)
+        {
+            HitPoints = 10;
+        }
+
+        public void Hit(int maxDamage, Random random)
+        {
+            HitPoints -= random.Next(1, maxDamage);
+        }
+
+        public void IncreaseHealth(int maxValue, Random random)
+        {
+            HitPoints += random.Next(1, maxValue);
+        }
+
+        public void Equip(string weaponName)
+        {
+            foreach (Weapon weapon in inventory)
+                if (weapon.Name == weaponName)
+                    equippedWeapon = weapon;
+        }
+
+        public void Move(Direction direction)
+        {
+            base.location = base.Move(direction, game.Boundaries);
+            if (!game.WeaponInRoom.PickedUp)
+                if (Nearby(game.WeaponInRoom.Location, 1))
+                {
+                    inventory.Add(game.WeaponInRoom);
+                    Equip(game.WeaponInRoom.Name);
+                }
+        }
+
+        public void Attack(Direction direction, Random random)
+        {
+            if (equippedWeapon == null)
+                return;
+            if (equippedWeapon is IPotion)
+            {
+                equippedWeapon.Attack(direction, random);
+                inventory.Remove(equippedWeapon);
+                equippedWeapon = null;
+            }
+            else
+                equippedWeapon.Attack();
+               
+        }
     }
 }
